57c8439b79d0fd8fb34a0e4ca355b97db3840a56
juanCamilo <juanmu@bextsa.com>
2022-09-13 18:10:01 -0500

    Chore: Added main form using Formik library

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, NavLink, Routes, Route, Navigate} from "react-router-dom";
 import logo from '../logo.svg';
 import {RegisterPage} from "../forms/pages/RegisterPage";
+import FormikBasicPage from "../forms/pages/FormikBasicPage";
 
 export const Navigation = () => {
     return (
@@ -13,7 +14,7 @@ export const Navigation = () => {
                             <NavLink to="/register" className={({isActive}) => isActive ? 'nav-active' : ''}>SignUp</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about" className={({isActive}) => isActive ? 'nav-active' : ''}>About</NavLink>
+                            <NavLink to="/formik-basic" className={({isActive}) => isActive ? 'nav-active' : ''}>Formik Basic</NavLink>
                         </li>
                         <li>
                             <NavLink to="/users" className={({isActive}) => isActive ? 'nav-active' : ''}>Users</NavLink>
@@ -21,9 +22,9 @@ export const Navigation = () => {
                     </ul>
                 </nav>
                 <Routes>
-                    <Route path="about" element={<h1>About</h1>}/>
-                    <Route path="users" element={<h1>Users</h1>}/>
-                    <Route path="register" element={<RegisterPage />}/>
+                    <Route path="formik-basic" element={<FormikBasicPage />} />
+                    <Route path="users" element={<h1>Users</h1>} />
+                    <Route path="register" element={<RegisterPage />} />
 
                     {/*Rutas no validas*/}
                     <Route path={"/*"} element={<Navigate to="/register" replace/>} />
