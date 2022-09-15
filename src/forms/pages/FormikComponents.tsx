efd5e3d8ba095a204c5a1055c5010768c21058ba
juanCamilo <camilo.murcia.pimentel@gmail.com>
2022-09-14 21:46:36 -0500

    Chore: Added selects and checks validations

--- src/forms/pages/FormikComponents.tsx
+++ src/forms/pages/FormikComponents.tsx
@@ -12,6 +12,8 @@ const FormikComponents = () => {
                     firstName: '',
                     lastName: '',
                     email: '',
+                    terms: false,
+                    jobType: '',
                 }}
                 onSubmit={(values) => {
                     console.log(values)
@@ -19,7 +21,9 @@ const FormikComponents = () => {
                 validationSchema={Yup.object({
                     firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
                     lastName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
-                    email: Yup.string().required().email('Invalid email')
+                    email: Yup.string().required().email('Invalid email'),
+                    terms: Yup.boolean().oneOf([true], 'This field is required'),
+                    jobType: Yup.string().required().notOneOf(["Manager"], "This option isn´t valid")
                 })}
             >
                 {
@@ -27,15 +31,31 @@ const FormikComponents = () => {
                         <Form noValidate>
                             <label htmlFor="firstName">First Name</label>
                             <Field name="firstName" type="text"/>
-                            <ErrorMessage name="firstName" component="spam"/>
+                            <ErrorMessage name="firstName" component="span"/>
 
                             <label htmlFor="lastName">Last Name</label>
                             <Field name="lastName" type="text"/>
-                            <ErrorMessage name="lastName" component="spam"/>
+                            <ErrorMessage name="lastName" component="span"/>
 
                             <label htmlFor="email">Email</label>
                             <Field name="email" type="text"/>
-                            <ErrorMessage name="email" component="spam"/>
+                            <ErrorMessage name="email" component="span"/>
+
+                            <label htmlFor="jobType">Job title</label>
+                            <Field name="jobType" as="select">
+                                <option value="">Pick a rol</option>
+                                <option value="Developer">Developer</option>
+                                <option value="Designer">Designer</option>
+                                <option value="Manager">Manager</option>
+                                <option value="CTO">CTO</option>
+                            </Field>
+                            <ErrorMessage name="jobType" component="span"/>
+
+                            <label>
+                                <Field name="terms" type="checkbox"/>
+                                Terms and Conditions
+                            </label>
+                            <ErrorMessage name="terms" component="span"/>
 
                             <button type="submit">Submit</button>
                         </Form>
